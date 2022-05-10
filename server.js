7bdf142e63ba69dc886e2ddcbcc323c77ef6adc1
Joshua Rangai <rangaij0116@hotmail.com>
2022-05-09 19:02:51 -0500

    post new animals

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array
+app.use(express.urlencoded({ extended: true }));
+// parse JSON
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -43,7 +51,17 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-}
+};
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -62,6 +80,35 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // set id based on location put into array
+    req.body.id = animals.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    // send added animal to createNewAnimal function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+       return true;
+}
